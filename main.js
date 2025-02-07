1f5c339c7f829d1341d191f1e498711663650102
vlouvier <louvier.valentyn@gmail.com>
2025-02-07 18:05:34 +0100

    main.js

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ const scoreManager = new ScoreManager();*/
 import PlayerEntity from "./entities/PlayerEntity.js";
 import GameLogicSystem from "./systems/GameLogicSystem.js";
 import RenderSystem from "./systems/RenderSystem.js";
-import { generateGrid, findPlayerPosition } from "./utils/tileMap.js";
 import MovementSystem from "./systems/MovementSystem.js";
+import { generateGrid, findPlayerPosition } from "./utils/tileMap.js";
 
 generateGrid();
 let { x, y } = findPlayerPosition();
@@ -18,12 +18,15 @@ const player = new PlayerEntity(0, x * 64, y * 64); // Initialize player entity 
 gameLogicSystem.addEntity(player);
 gameLogicSystem.addSystem(new MovementSystem());
 gameLogicSystem.addSystem(new RenderSystem());
+
 console.log(gameLogicSystem);
 
 function gameLoop() {
   gameLogicSystem.update();
+
   // requestAnimationFrame(gameLoop);
 }
+
 console.log("player position on the map", findPlayerPosition());
 
 requestAnimationFrame(gameLoop);
